1b8902f059c36adccfe2e7c3acdc4b0320512980
Kieron Garvey <garveykieron@gmail.com>
2024-07-23 22:01:10 +0100

    Added Firebase Auth, Firestore DB and Storage with Create Movie

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -4,16 +4,6 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDHu4GOUNzmEDTfxVWBJq_PcelhEey8uBs",
-//   authDomain: "movieapp-c93cf.firebaseapp.com",
-//   projectId: "movieapp-c93cf",
-//   storageBucket: "movieapp-c93cf.appspot.com",
-//   messagingSenderId: "1050616331205",
-//   appId: "1:1050616331205:web:ce837f3a0220c643bb69ac",
-//   measurementId: "G-FKRZEE0ZRH"
-// };
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
@@ -23,16 +13,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
-
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId:  process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID
-// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
