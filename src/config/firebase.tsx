b607fc15ec6814240983fab0d1f456530ff896b3
Kieron Garvey <garveykieron@gmail.com>
2024-07-23 22:01:26 +0100

    Added Firebase Auth, Firestore DB and Storage with Create Movie

--- src/config/firebase.tsx
+++ src/config/firebase.tsx
@@ -4,10 +4,11 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId:  import.meta.env.VITE_ROJECT_ID,
+  projectId: import.meta.env.VITE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
